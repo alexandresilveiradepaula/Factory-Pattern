1df0cdb9c58e8a9faab8d54dc37a8a045eb46473
Alexandre Silveira de Paula <168464775+alexandresilveiradepaula@users.noreply.github.com>
2024-10-09 14:34:31 -0300

    Criação da TConnection.class
    Criação da classe de conexão do PDO

--- TConnection.class.php
+++ TConnection.class.php
@@ -24,10 +24,60 @@
             
             $db = parse_ini_file("app.config/{$name}.ini");
         }
+        else 
+        {
+            // se não existir, lança um erro
+            throw new Exception("Arquivo.'$name' não encontrado");
+        }
 
+        // lê os arquivos e grava as informações contidas neles
+        $user = isset($db['user']) ? $db['user'] :'NULL';
+        $pass = isset($db['pass']) ? $db['pass'] :'NULL';
+        $name = isset($db['name']) ? $db['name'] :'NULL';
+        $host = isset($db['host']) ? $db['host'] :'NULL';
+        $type = isset($db['type']) ? $db['type'] :'NULL';
+        $port = isset($db['port']) ? $db['port']: 'NULL';
+
+    
+
+    // descobre qual o tipo de (driver) banco de dados a ser utilizado
+
+    switch ($type){
+        case 'pgsql':
+            $port = $port ? $port: '5432';
+
+            $conn = new PDO("pgsql:dbname={$name}; user={$user}: password={$pass}; host = $host; port = {$port}");
+            break;
+
+        case 'mysql':
+            $port = $port ? $port : '3306';
+
+            $conn = new PDO("mysql:host={$host};port={$port}; dbname={$name}",$user,$pass);
+            break;
+        
+        case 'ibase':
+            $conn = new PDO("firebird:dbname={$name}",$user,$pass);
+            break;
+
+        case 'oci8':
+            $conn = new PDO("oci8:dbname={$name}",$user,$pass);
+            break;
+
+        case 'mssql':
+            $conn = new PDO("mssql:host={$host},1433;dbname={$name}",$user,$pass);
+            break;
+        }
+
+        //define para que o PDO lance excessões na ocorrencia de erros
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // retorna o objeto instanciado
+
+        return $conn;
     }
-
  }
+
  
  
 
